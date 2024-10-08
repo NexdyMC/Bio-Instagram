4ae66da19749f06a5e9e0d04c9cf450dbd710e55
NexdyMC <febripratama1786@gmail.com>
2024-10-08 22:46:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+const youtubr = document.getElementById("youtuber");
+const instagraam = document.getElementById("instagram");
+const Tiktok = document.getElementById("tiktok");
+const github = document.getElementById("github"); 
+const out = new Date();
+const description = document.querySelector(".description");
+
+description.innerHTML = ["<p>" + /// --------- DISINGKAT BIAR JELAS --------- ///
+        "Hai Nama ku NexdyMC aku seorang kontent creator minecraft dan pembuat texture pack mini java dan bedrock" 
+        + "</p> "];
+
+function tanggal() {
+    const tanggal = document.getElementById("tanggal");
+    const haritext = ["minggu", "Senin", "Selasa", "Rabu", "Kamis", "jumat", "sabtu"];
+    const bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "juli", "Agustus", "September", "October", "November", "desember"];
+    const month = bulan[out.getMonth()];
+    const hari = haritext[out.getDay()];
+    const harinomer = String(out.getDate()).padStart(2, "0");
+    const tahun = String(out.getFullYear()).padStart(2, "0");
+
+    tanggal.textContent = hari + " " + harinomer + " " + month + " " + tahun ;
+    tanggal.style.fontSize = '1.5rem' ;
+}
+setInterval(tanggal, 1000);
+tanggal();
+
+function clock() {
+    const clock = document.getElementById("clock");
+    const jam = String(out.getHours()).padStart(2, "0");
+    const menit = String(out.getMinutes()).padStart(2, "0");
+    const detik = String(out.getSeconds()).padStart(2, "0")
+
+    clock.textContent = (jam + ':' + menit + ':' + detik);
+    clock.style.fontSize = '1.5rem' ;
+}
+setInterval(clock, 1000);
+clock();
+
